7486b3f794749041db02c61bc5ccbe1bfd35c524
bertRC <sithalbert@ya.ru>
2019-11-14 21:24:55 +0300

    ContainerProImpl class realized

--- src/main/java/ru/itpark/framework/container/ContainerProImpl.java
+++ src/main/java/ru/itpark/framework/container/ContainerProImpl.java
@@ -1,4 +1,60 @@
 package ru.itpark.framework.container;
 
-public class ContainerProImpl {
+import org.reflections.Reflections;
+import ru.itpark.framework.annotation.Component;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ContainerProImpl implements Container {
+
+    private Optional<Constructor<?>> getFirstSuitableConstructor(Class<?> type, Set<Class> setOfComponents) {
+        return Arrays.stream(type.getConstructors())
+                .filter(c -> {
+                    Class<?>[] parameterTypes = c.getParameterTypes();
+                    return setOfComponents.containsAll(Arrays.asList(parameterTypes));
+                }).findFirst();
+    }
+
+    @Override
+    public Map<Class, Object> init() {
+        final Reflections reflections = new Reflections();
+        final Map<Class, Object> components = new HashMap<>();
+
+        final Set<Class<?>> types = reflections.getTypesAnnotatedWith(Component.class, true).stream()
+                .filter(o -> !o.isAnnotation()).collect(Collectors.toSet());
+
+        while (!types.equals(components.keySet())) {
+            Map<Class, Object> currentGeneration = types.stream()
+                    .filter(o -> !components.containsKey(o))
+                    .filter(o -> getFirstSuitableConstructor(o, components.keySet()).isPresent())
+                    .collect(Collectors.toMap(o -> o, o -> {
+                        final Constructor<?> constructor = getFirstSuitableConstructor(o, components.keySet()).get();
+                        final Object[] params = Arrays.stream(constructor.getParameterTypes())
+                                .map(components::get).toArray();
+                        try {
+                            return constructor.newInstance(params);
+                        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                            throw new RuntimeException(e);
+                        }
+                    }));
+
+            if (currentGeneration.isEmpty()) {
+                throw new RuntimeException("The following components were not found: "
+                        + new HashSet<>(types).removeAll(components.keySet()));
+            }
+            components.putAll(currentGeneration);
+            System.out.println(currentGeneration.keySet());
+        }
+
+        return components;
+    }
+
+    public static void main(String[] args) {
+        final Container container = new ContainerProImpl();
+        container.init();
+    }
 }
