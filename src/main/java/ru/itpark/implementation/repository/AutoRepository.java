908e13b2f96ff267e55946ff97ff106362fd2315
bertRC <sithalbert@ya.ru>
2019-11-15 19:19:09 +0300

    some modifications

--- src/main/java/ru/itpark/implementation/repository/AutoRepository.java
+++ src/main/java/ru/itpark/implementation/repository/AutoRepository.java
@@ -1,10 +1,21 @@
 package ru.itpark.implementation.repository;
 
 import ru.itpark.framework.annotation.Component;
+import ru.itpark.implementation.util.JdbcTemplate;
+import ru.itpark.implementation.util.ResourcesPaths;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 @Component
 public class AutoRepository {
-    public AutoRepository() {
-        System.out.println("*Autorepository created*");
+    private final JdbcTemplate jdbcTemplate;
+    private final DataSource ds;
+
+    public AutoRepository(JdbcTemplate jdbcTemplate) throws NamingException {
+        this.jdbcTemplate = jdbcTemplate;
+        InitialContext context = new InitialContext();
+        ds = (DataSource) context.lookup(ResourcesPaths.dbPath);
     }
 }
