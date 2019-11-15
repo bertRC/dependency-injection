2662aed599db2c7f2fa316a4397d5b830fc5d544
bertRC <sithalbert@ya.ru>
2019-11-15 16:07:02 +0300

    ContainerDefaultImpl realized

--- src/main/java/ru/itpark/framework/container/ContainerDefaultImpl.java
+++ src/main/java/ru/itpark/framework/container/ContainerDefaultImpl.java
@@ -16,125 +16,44 @@ public class ContainerDefaultImpl implements Container {
 
         final Set<Class<?>> types = reflections.getTypesAnnotatedWith(Component.class, true).stream()
                 .filter(o -> !o.isAnnotation()).collect(Collectors.toSet());
-        System.out.println("--- Всё что есть с аннотациями @Component ---");
-        System.out.println(types);
 
-        final List<Class<?>> simple = types.stream().filter(o -> {
-            if (o.getConstructors().length != 1) {
-                throw new RuntimeException("invalid count of constructors: " + o.getName());
+        while (!types.equals(components.keySet())) {
+            Map<Class, Object> currentGeneration = types.stream()
+                    .filter(o -> !components.containsKey(o))
+                    .filter(o -> {
+                        final Constructor<?>[] constructors = o.getConstructors();
+                        if (constructors.length != 1) {
+                            throw new RuntimeException("Ambiguous constructors!");
+                        }
+
+                        final Class<?>[] parameterTypes = constructors[0].getParameterTypes();
+                        return components.keySet().containsAll(Arrays.asList(parameterTypes));
+                    })
+                    .collect(Collectors.toMap(o -> o, o -> {
+                        final Constructor<?> constructor = o.getConstructors()[0];
+                        final Object[] params = Arrays.stream(constructor.getParameterTypes())
+                                .map(components::get).toArray();
+                        try {
+                            return constructor.newInstance(params);
+                        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                            e.printStackTrace();
+                            throw new RuntimeException(e);
+                        }
+                    }));
+
+            if (currentGeneration.isEmpty()) {
+                HashSet<Class<?>> missingComponents = new HashSet<>(types);
+                missingComponents.removeAll(components.keySet());
+                throw new RuntimeException("Can not create instances of the following components: "
+                        + missingComponents);
             }
+            components.putAll(currentGeneration);
+        }
 
-            final int parameterCount = o.getConstructors()[0].getParameterCount();
-            return parameterCount == 0;
-        }).collect(Collectors.toList());
+        if (components.isEmpty()) {
+            throw new RuntimeException("There is no components");
+        }
 
-        System.out.println("--- Классы с простым конструктором ---");
-        System.out.println(simple);
-
-        // components -> Class vs Object
-        // map
-        System.out.println("--- Первое поколение ---");
-        final Map<Class, Object> firstGeneration = simple.stream()
-                .map(o -> {
-                    try {
-                        return o.getConstructor().newInstance();
-                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-                        e.printStackTrace();
-                        throw new RuntimeException("invalid count of constructors", e);
-                    }
-                })
-                .collect(Collectors.toMap(o -> o.getClass(), o -> o));
-        components.putAll(firstGeneration);
-        System.out.println(firstGeneration);
-
-        System.out.println("--- Второе поколение ---");
-        Map<Class, Object> secondGeneration = types.stream()
-                .filter(o -> !components.containsKey(o))
-                .filter(o -> {
-                    final Constructor<?>[] constructors = o.getConstructors();
-                    if (constructors.length != 1) {
-                        throw new RuntimeException("Ambiguous constructors!");
-                    }
-
-                    final Class<?>[] parameterTypes = constructors[0].getParameterTypes();
-                    return components.keySet().containsAll(Arrays.asList(parameterTypes));
-                })
-                .collect(Collectors.toMap(o -> o, o -> {
-                    // AutoService (AutoRepository repository)
-                    final Constructor<?> constructor = o.getConstructors()[0];
-                    final Object[] params = Arrays.stream(constructor.getParameterTypes())
-                            .map(components::get).toArray();
-                    try {
-                        return constructor.newInstance(params);
-                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                        e.printStackTrace();
-                        throw new RuntimeException();
-                    }
-                }));
-        components.putAll(secondGeneration);
-        System.out.println(secondGeneration);
-
-        System.out.println("--- Третье поколение ---");
-        Map<Class, Object> thirdGeneration = types.stream()
-                .filter(o -> !components.containsKey(o))
-                .filter(o -> {
-                    final Constructor<?>[] constructors = o.getConstructors();
-                    if (constructors.length != 1) {
-                        throw new RuntimeException("Ambiguous constructors!");
-                    }
-
-                    final Class<?>[] parameterTypes = constructors[0].getParameterTypes();
-                    return components.keySet().containsAll(Arrays.asList(parameterTypes));
-                })
-                .collect(Collectors.toMap(o -> o, o -> {
-                    // AutoService (AutoRepository repository)
-                    final Constructor<?> constructor = o.getConstructors()[0];
-                    final Object[] params = Arrays.stream(constructor.getParameterTypes())
-                            .map(components::get).toArray();
-                    try {
-                        return constructor.newInstance(params);
-                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                        e.printStackTrace();
-                        throw new RuntimeException();
-                    }
-                }));
-        components.putAll(thirdGeneration);
-        System.out.println(thirdGeneration);
-
-        System.out.println("--- Четвертое поколение ---");
-        Map<Class, Object> fourthGeneration = types.stream()
-                .filter(o -> !components.containsKey(o))
-                .filter(o -> {
-                    final Constructor<?>[] constructors = o.getConstructors();
-                    if (constructors.length != 1) {
-                        throw new RuntimeException("Ambiguous constructors!");
-                    }
-
-                    final Class<?>[] parameterTypes = constructors[0].getParameterTypes();
-                    return components.keySet().containsAll(Arrays.asList(parameterTypes));
-                })
-                .collect(Collectors.toMap(o -> o, o -> {
-                    // AutoService (AutoRepository repository)
-                    final Constructor<?> constructor = o.getConstructors()[0];
-                    final Object[] params = Arrays.stream(constructor.getParameterTypes())
-                            .map(components::get).toArray();
-                    try {
-                        return constructor.newInstance(params);
-                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                        e.printStackTrace();
-                        throw new RuntimeException();
-                    }
-                }));
-        components.putAll(fourthGeneration);
-        System.out.println(fourthGeneration);
-
-        System.out.println("--- Найденные компоненты ---");
-        System.out.println(components);
         return components;
-    }
-
-    public static void main(String[] args) {
-        final Container container = new ContainerDefaultImpl();
-        final Map<Class, Object> components = container.init();
     }
 }
