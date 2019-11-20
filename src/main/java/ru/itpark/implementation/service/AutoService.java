56335594d61c3fce1a2e39127fb372169b4f2e6b
bertRC <sithalbert@ya.ru>
2019-11-20 19:34:06 +0300

    getById, create and removeById methods added

--- src/main/java/ru/itpark/implementation/service/AutoService.java
+++ src/main/java/ru/itpark/implementation/service/AutoService.java
@@ -1,23 +1,39 @@
 package ru.itpark.implementation.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+import ru.itpark.exceptions.NotFoundException;
 import ru.itpark.framework.annotation.Component;
 import ru.itpark.domain.Auto;
 import ru.itpark.implementation.repository.AutoRepository;
 
+import javax.servlet.http.Part;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class AutoService {
     private final AutoRepository repository;
+    private final FileService fileService;
 
     public List<Auto> getAll() {
         return repository.getAll();
     }
 
-    public Auto create(Auto auto) {
-        return repository.create(auto);
+    public Auto getById(long id) {
+        return repository.getById(id).orElseThrow(() -> new NotFoundException(String.format("Object with id %d not found", id)));
+    }
+
+    public void create(Auto auto, Part part) {
+        if (part != null) {
+            val image = fileService.writeFile(part);
+            auto.setImage(image);
+        }
+        repository.create(auto);
+    }
+
+    public boolean removeById(long id) {
+        return fileService.removeFile(repository.removeById(id));
     }
 
     public List<Auto> search(String text) {
