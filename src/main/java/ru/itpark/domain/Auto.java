af32e644e8f3b390aba460ffec1fb1bf1d61f1e8
bertRC <sithalbert@ya.ru>
2019-11-20 19:52:13 +0300

    some modifications

--- src/main/java/ru/itpark/domain/Auto.java
+++ src/main/java/ru/itpark/domain/Auto.java
@@ -2,11 +2,9 @@ package ru.itpark.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Auto {
     private long id;
     private String name;
